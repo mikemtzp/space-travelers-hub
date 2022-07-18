e0cd0cafefadcde7b6a3f2600f207ef1bc9a0681
mikemtzp <mikemp8226@gmail.com>
2022-07-18 17:28:28 -0500

    Add Navlink to add style to selected page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,31 +1,47 @@
+import * as React from 'react';
 import { NavLink } from 'react-router-dom';
 import logo from '../assets/images/planet.png';
 
-const Navbar = () => (
-  <div>
+const Navbar = () => {
+  const activeClassName = 'underline';
+  return (
     <div>
-      <div className="Logo">
-        <img
-          src={logo}
-          alt="planet"
-        />
-        <p>Space Travelers Hub</p>
+      <div>
+        <div className="Logo">
+          <img src={logo} alt="planet" />
+          <p>Space Travelers Hub</p>
+        </div>
       </div>
+      <nav>
+        <ul className="">
+          <li>
+            <NavLink
+              to="/rockets"
+              className={({ isActive }) => (isActive ? activeClassName : undefined)}
+            >
+              Rockets
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/missions"
+              className={({ isActive }) => (isActive ? activeClassName : undefined)}
+            >
+              Missions
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/profile"
+              className={({ isActive }) => (isActive ? activeClassName : undefined)}
+            >
+              My Profiles
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
     </div>
-    <nav>
-      <ul className="">
-        <li>
-          <NavLink to="/rockets"> Rockets </NavLink>
-        </li>
-        <li>
-          <NavLink to="/missions"> Missions </NavLink>
-        </li>
-        <li>
-          <NavLink to="/profile"> My Profiles </NavLink>
-        </li>
-      </ul>
-    </nav>
-  </div>
-);
+  );
+};
 
 export default Navbar;
