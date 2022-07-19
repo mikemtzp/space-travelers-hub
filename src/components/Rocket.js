16946210448800cc08f28039889dda942203f00a
mikemtzp <mikemp8226@gmail.com>
2022-07-19 12:35:26 -0500

    Create Rocket component

---
+++ src/components/Rocket.js
@@ -0,0 +1,12 @@
+const Rocket = () => (
+  <div className="rocket-container">
+    <img className="rocket-container__img" src="https://www.nasa.gov/sites/default/files/styles/full_width/public/thumbnails/image/edu_what_is_sls_nasa_knows.jpg?itok=DIZU8vD0" alt="" />
+    <div className="rocket-info">
+      <h2 className="rocket-info__title">Rocket</h2>
+      <p className="rocket-info__description">lorem ipsum</p>
+      <button className="rocket-info__button" type="button">Test</button>
+    </div>
+  </div>
+);
+
+export default Rocket;
