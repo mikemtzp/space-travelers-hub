e6ae09bc50019adeb81e6b405c0ff8785915d3f1
Zeraltz <96924302+Zeraltz@users.noreply.github.com>
2022-07-22 11:01:11 -0500

    Add mission tests

--- src/__tests__/missions.test.js
+++ src/__tests__/missions.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { Provider } from 'react-redux';
 import store from '../redux/configureStore';
